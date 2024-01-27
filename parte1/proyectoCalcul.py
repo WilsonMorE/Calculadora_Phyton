3ad02c38bfede4a949af25a8e981fcf0d4c79c10
WilsonMorE <157905679+WilsonMorE@users.noreply.github.com>
2024-01-27 11:25:33 -0600

    Update proyectoCalcul.py

--- parte1/proyectoCalcul.py
+++ parte1/proyectoCalcul.py
@@ -7,4 +7,27 @@ ventana.title("Calculadora")
 e_texto = Entry(ventana, font= ("Calibri 20"))
 e_texto.grid(row = 0, column= 0, columnspan= 4, padx =5, pady=5)
 
+#Botones
+boton1 = Button(ventana, text= "1", width= 5, height=2)
+boton2 = Button(ventana, text= "2", width= 5, height=2)
+boton3 = Button(ventana, text= "3", width= 5, height=2)
+boton4 = Button(ventana, text= "4", width= 5, height=2)
+boton5 = Button(ventana, text= "5", width= 5, height=2)
+boton6 = Button(ventana, text= "6", width= 5, height=2)
+boton7 = Button(ventana, text= "7", width= 5, height=2)
+boton8 = Button(ventana, text= "8", width= 5, height=2)
+boton9 = Button(ventana, text= "9", width= 5, height=2)
+boton0 = Button(ventana, text= "0", width= 13, height=2)
+
+boton_parentesis1 = Button(ventana, text= "(", width= 5, height=2)
+boton_del = Button(ventana, text= "AC", width= 5, height=2)
+boton_parentesis2 = Button(ventana, text= ")", width= 5, height=2)
+boton_punto = Button(ventana, text= ".", width= 5, height=2)
+
+boton_mul = Button(ventana, text= "x", width= 5, height=2)
+boton_div = Button(ventana, text= "/", width= 5, height=2)
+boton_sum = Button(ventana, text= "+", width= 5, height=2)
+boton_res = Button(ventana, text= "-", width= 5, height=2)
+boton_igual = Button(ventana, text= "=", width= 5, height=2)
+
 ventana.mainloop()
