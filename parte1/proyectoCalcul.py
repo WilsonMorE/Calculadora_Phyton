421dbeb0d9d5e1282e5327db0fbf42be3c4b7423
WilsonMorE <157905679+WilsonMorE@users.noreply.github.com>
2024-01-28 14:20:12 -0600

    Update proyectoCalcul.py

--- parte1/proyectoCalcul.py
+++ parte1/proyectoCalcul.py
@@ -30,4 +30,30 @@ boton_sum = Button(ventana, text= "+", width= 5, height=2)
 boton_res = Button(ventana, text= "-", width= 5, height=2)
 boton_igual = Button(ventana, text= "=", width= 5, height=2)
 
+#ouput de botones
+boton_del.grid(row=1 , column =0, padx = 5, pady = 5)
+boton_parentesis1.grid(row=1 , column = 1, padx = 5, pady = 5)
+boton_parentesis2.grid(row=1 , column = 2, padx = 5, pady = 5)
+boton_div.grid(row=1 , column =3 , padx = 5, pady = 5)
+
+
+boton7.grid(row=2 , column = 0, padx = 5, pady = 5)
+boton8.grid(row=2 , column =1 , padx = 5, pady = 5)
+boton9.grid(row=2 , column = 2, padx = 5, pady = 5)
+boton_mul.grid(row= 2, column = 3, padx = 5, pady = 5)
+
+boton6.grid(row= 3, column = 0, padx = 5, pady = 5)
+boton5.grid(row= 3, column = 1, padx = 5, pady = 5)
+boton4.grid(row= 3, column = 2, padx = 5, pady = 5)
+boton_sum.grid(row= 3, column = 3, padx = 5, pady = 5)
+
+boton3.grid(row= 4, column = 0, padx = 5, pady = 5)
+boton2.grid(row= 4, column = 1, padx = 5, pady = 5)
+boton1.grid(row= 4, column = 2, padx = 5, pady = 5)
+boton_res.grid(row= 4, column = 3, padx = 5, pady = 5)
+
+boton0.grid(row= 5, column = 0,columnspan = 2, padx = 5, pady = 5)
+boton_punto.grid(row= 5, column = 2, padx = 5, pady = 5)
+boton_igual.grid(row= 5, column = 3, padx = 5, pady = 5)
+
 ventana.mainloop()
